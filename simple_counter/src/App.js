31647fb4b4a65c3eaea644f281da8bbc14eacf5f
NicolasArayaB <nicolasaraya@gmail.com>
2021-03-07 18:00:48 +0000

    impressition fixed

--- simple_counter/src/App.js
+++ simple_counter/src/App.js
@@ -1,20 +1,13 @@
-import './App.css';
-import SecondsCounter from './components/SecondsCounter.js'
-import { Container, Row, Col } from "react-bootstrap"
+import SecondsCounter from './components/SecondsCounter.js';
+import { Container } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Container>
-            <Row>
-                <Col><h1><i className="far fa-clock" /></h1></Col>
-                <Col><SecondsCounter multiplier="10065000" /></Col>
-                <Col><SecondsCounter multiplier="1006500" /></Col>
-                <Col><SecondsCounter multiplier="100650" /></Col>
-                <Col><SecondsCounter multiplier="10065" /></Col>
-                <Col><SecondsCounter multiplier="1000" /></Col>
-            </Row>
+            <SecondsCounter />
         </Container>
     </div>
   );
--- simple_counter/src/components/SecondsCounter.js
+++ simple_counter/src/components/SecondsCounter.js
@@ -1,16 +1,31 @@
 import React, { useState, useEffect } from "react";
+import { Col, Row } from "react-bootstrap";
 
-const SecondsCounter = (props) => {
-    const [seconds, setSeconds] = useState(0);
+const SecondsCounter = () => {
     
+    const [counter, setCounter] = useState(0);
     useEffect(() => {
-        const num = setInterval(() => {
-            setSeconds(s => s + 1);
-        }, props.multiplier);
-        return () => clearInterval(num);
-    });
+        const interval = setInterval(() => {
+            setCounter(counter => counter + 1)
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+    
+    let count1 = Math.floor(counter / 1 % 10)
+    let count2 = Math.floor(counter / 10 % 10)
+    let count3 = Math.floor(counter / 100 % 10)
+    let count4 = Math.floor(counter / 1000 % 10)
+    let count5 = Math.floor(counter / 10000 % 10)
+    return (
+    <Row>
+        <Col><h1><i className="far fa-clock" /></h1></Col>
+        <Col><h1>{count5}</h1></Col>
+        <Col><h1>{count4}</h1></Col>
+        <Col><h1>{count3}</h1></Col>
+        <Col><h1>{count2}</h1></Col>
+        <Col><h1>{count1}</h1></Col>
+    </Row>)
+};
 
-    return seconds < 10 ? <h1>{seconds}</h1> : setSeconds(0)
-}
 
 export default SecondsCounter;
